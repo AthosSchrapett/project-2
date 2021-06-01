20a7278954bfd2acefbf8cb7e9c467b6bbfd099e
athosschrapett <athosschrapett@outlook.com>
2021-05-31 21:31:40 -0300

    Use Effect Final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg'; */
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const eventFn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  //componentDidUpdate - Executa toda vez que o componente atualiza
+  /* useEffect(() => {
+    console.log('componentDidUpdate');
+  }); */
+
+  //componentDidMount - Executa 1x
+  useEffect(() => {
+    document.querySelector('h1')?.addEventListener('click', eventFn);
+
+    //componentWillUmount - Limpeza
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventFn);
+    };
+  }, []);
+
+  //Com dependência - executa toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('C1:', counter, 'C2:', counter2);
+  }, [counter, counter2]);
+
+  return (
+    <div className="App">
+      <p>Teste 1</p>
+      <h1>
+        C1: {counter} C2: {counter2}{' '}
+      </h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>++</button>
+    </div>
+  );
+}
+
+/* function App() {
   const [reverse, setReverse] = useState(false);
   const [counter, setCounter] = useState(0);
   const reverseClass = reverse ? 'reverse' : '';
@@ -34,6 +74,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
