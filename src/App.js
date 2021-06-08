1276ca61b343a2ba52eee16b3b74cd9967ca0c0f
athosschrapett <athosschrapett@outlook.com>
2021-06-08 08:11:27 -0300

    Iniciando useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useReducer } from 'react';
 import './App.css';
-import { Div } from './components/Div';
-import { AppContext } from './contexts/AppContext';
+
+const globalState = {
+  title: 'O titulo do contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('chamou inverter');
+      const { title } = state;
+      return {
+        ...state,
+        title: title.split('').reverse().join(''),
+      };
+    }
+  }
+  console.log('Nenhuma Action Encontrada');
+  return { ...state };
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+  console.log(body);
   return (
-    <AppContext>
-      <Div></Div>
-    </AppContext>
+    <div>
+      <h1>
+        {title} - {counter}
+      </h1>
+      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleString('pt-br') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+      <button onClick={() => dispatch({ type: '' })}>Nenhuma Action</button>
+    </div>
   );
 }
 
